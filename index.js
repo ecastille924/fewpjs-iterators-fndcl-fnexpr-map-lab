b459e0dff2eb2070ca065765ebc30a0923a8a412
Erick Castille <ecastille924@gmail.com>
2021-07-05 17:22:15 -0400

    commit tree c933de1163cbb26fd5bc765a07a27123af1d0c31

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tutorials = [
 
 
 
-const titleCased = (array) => {
+const titleCased = () => {
   return tutorials.map( string => {
     let words = string.split(' ')
     let capitalizedWords = words.map( word => word.charAt(0).toUpperCase() + word.slice(1))
@@ -21,3 +21,16 @@ const titleCased = (array) => {
     return result
   })
 }
+
+// 1. seperate all words in string 
+// 2. capitalize first letter of each word 
+// 3. join words back together 
+
+// function titleCased(tutorials){
+//   return tutorials.map( string => {
+//     let words = string.split(' ')
+//     let capitalizedWords = words.map( word => word.charAt(0).toUpperCase() + word.slice(1))
+//     let result = capitalizedWords.join(' ')
+//     return result
+//   })
+// }
